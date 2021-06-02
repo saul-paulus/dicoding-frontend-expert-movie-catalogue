fd693ab777c3840d1a932a667156d665776051a0
saul-paulus <saulpaulus17@icloud.com>
2021-06-02 15:37:07 +0900

    memperbaiki favorite templates

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,11 @@ const CONFIG = {
   KEY: '32062d646a275282ed018d26015c22ec',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
-  DEFAULT_LANGUAGE: 'en-us'
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies'
 }
 
 export default CONFIG
